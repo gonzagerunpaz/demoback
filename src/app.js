df20d3827abb5abd25d16b19ac14eb8b7d72dc0f
shadyrecords@live.com <shadyrecords@live.com>
2022-05-02 20:46:40 -0300

    Cambie el puerto por defecto

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 
 /* Esto es para que interprete que la informacion que me llege en el cuerpo del request es json*/
 app.use(express.json())
